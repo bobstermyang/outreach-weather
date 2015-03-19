502066e9e1cc5c7dc7d3c4d2783459c36e147b4a
Bobby Yang <bobstermyang@gmail.com>
2015-03-19 16:08:09 -0600

    Update connect.php

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,9 +1,9 @@
 <?php
 // Connection parameters
-$DatabaseServer = 'localhost';
-$DatabaseUser   = 'root';
-$DatabasePass   = 'Para123&';
-$DatabaseName   = 'weather';
+$DatabaseServer = 'us-cdbr-iron-east-02.cleardb.net';
+$DatabaseUser   = 'be1045518475c0';
+$DatabasePass   = '71a38501';
+$DatabaseName   = 'heroku_59e0169dc2f2121';
 
 // Connecting to the database
 $database = new mysqli($DatabaseServer, $DatabaseUser, $DatabasePass, $DatabaseName);
